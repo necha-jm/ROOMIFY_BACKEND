f75813e27ed462b3f35657d81f28f695bf6b82df
necha-jm <12345678+necha-jm@users.noreply.github.com>
2026-04-12 09:23:08 +0300

    Initial commi

--- src/main/java/com/ROOMIFY/Roomify/config/FirebaseConfig.java
+++ src/main/java/com/ROOMIFY/Roomify/config/FirebaseConfig.java
@@ -1,32 +1,37 @@
 package com.ROOMIFY.Roomify.config;
-
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import jakarta.annotation.PostConstruct;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.stereotype.Component;
 
-import java.io.FileInputStream;
+import java.io.IOException;
 
-@Configuration
+@Component
 public class FirebaseConfig {
 
     @PostConstruct
     public void init() {
         try {
-            FileInputStream serviceAccount =
-                    new FileInputStream("firebase-service-account.json");
-
-            FirebaseOptions options = FirebaseOptions.builder()
-                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-                    .build();
-
             if (FirebaseApp.getApps().isEmpty()) {
+
+                GoogleCredentials credentials =
+                        GoogleCredentials.fromStream(
+                                new ClassPathResource("firebase-service-account.json").getInputStream()
+                        );
+
+                FirebaseOptions options = FirebaseOptions.builder()
+                        .setCredentials(credentials)
+                        .build();
+
                 FirebaseApp.initializeApp(options);
+
+                System.out.println("Firebase initialized successfully");
             }
 
-        } catch (Exception e) {
-            e.printStackTrace();
+        } catch (IOException e) {
+            throw new RuntimeException("Firebase initialization failed", e);
         }
     }
 }
