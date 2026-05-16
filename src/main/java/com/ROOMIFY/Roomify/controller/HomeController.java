5b02747679b2c631b1100bda415dc1e603e2c9e9
necha-jm <charlesnestory432@gmail.com>
2026-05-16 05:21:16 +0000

    Fix JWT secret configuration

---
+++ src/main/java/com/ROOMIFY/Roomify/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.ROOMIFY.Roomify.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Roomify Backend Running";
+    }
+}
